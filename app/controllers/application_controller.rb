5ac755362b834f938fd7649e985f3aa4b00b996e
Anthony <github email address>
2018-03-20 14:09:25 -0400

    added flash messages view and updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :owner?
+  def owner?(resource)
+    current_user && resource.user_id == current_user.id
+  end
+
+  def authorize_user!(resource)
+    if !owner?(resource)
+      flash[:alert] = "You are not allowed to change someone else's #{resource.model_name.to_s.downcase}."
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: "users/omniauth_callbacks" }
 
 root 'listings#index'
+  resources :listings do
+  resources :types
+  resources :reviews
+end
+
 get 'all', to: 'listings#index', as: 'all_listings'
 end
