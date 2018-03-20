a803caef0c8822f1cac684ae68f6f03482b20a9c
Anthony <github email address>
2018-03-20 12:58:03 -0400

    added listing controller and _form view

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,77 @@
+class ListingsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_action only: [:edit, :update, :destroy] { authorize_user!(@listing) }
+
+  def index
+    if request.path == '/all'
+      @listings = Listing.most_recently_updated
+    else
+      @listings = Listing.most_recently_updated.limit(9)
+    end
+  end
+
+  def show
+    @review = Review.new
+    @reviews = @review.most_recent_reviews
+  end
+
+  def new
+    @listing = Listing.new
+    @categories = Category.all
+    @listing.types.build
+    @new_category = @listing.categories.build
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    @listing.user_id = current_user.id
+
+    if @listing.save
+      flash[:notice] = 'Successfully created listing'
+      redirect_to @listing
+    else
+      @listing.types.build
+      render :new
+    end
+  end
+
+  def edit
+    @categories = Category.all
+    @new_category = @listing.categories.build
+  end
+
+  def update
+    if @listing.update(listing_params)
+      redirect_to @listing
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @listing.destroy
+    redirect_to root_path, notice: "Successfully deleted that listing"
+  end
+
+  private
+
+    def listing_params
+      params.require(:listing).permit(
+        :title,
+        :category,
+        :description,
+        :contact,
+        :cost,
+        :image,
+        types_attributes: [:id, :name, :_destroy],
+        category_ids: [],
+        categories_attributes: [:name]
+        )
+
+    end
+
+    def set_listing
+      @listing = Listing.find(params[:id])
+    end
+end
