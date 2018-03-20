6251593f533784603daf34efa5532771ef3115ac
Anthony <github email address>
2018-03-20 13:36:07 -0400

    added listing views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: "users/omniauth_callbacks" }
+
 root 'listings#index'
+get 'all', to: 'listings#index', as: 'all_listings'
 end
