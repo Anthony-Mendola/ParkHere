4cea6e47baa8c8485b8ee84f115bf5df90b52183
Anthony <github email address>
2018-03-20 18:18:31 -0400

    added a user controller and user home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: "users/omniauth_callbacks" }
-
+get '/users/home' => 'users#home', as: "user_home"
 root 'listings#index'
   resources :listings do
   resources :types
