f47171db92941c8e6787b0881b081cf2f414abb3
Anthony <github email address>
2018-03-20 11:59:47 -0400

    added models

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ApplicationRecord
+  has_many :listings
+  has_many :reviews
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      # user.skip_confirmation!
+    end
+  end
+
+  def apply_omniauth(auth)
+    update_attributes(
+      provider: auth.provider,
+      uid: auth.uid
+      )
+  end
+end
